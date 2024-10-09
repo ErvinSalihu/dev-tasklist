2c3711cbdaeef8196b39da5c8380329f2b976729
ESalihu564 <esalihu564@gmail.com>
2024-10-09 17:29:20 +0200

    Innital commit

--- backend/scripts/init.sql
+++ backend/scripts/init.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS bookings (
 );
 
 INSERT INTO bookings (service, doctor_name, start_time, end_time, date) VALUES
-    ('Service A', 'Dr. Smith', '09:00 AM', '10:00 AM', '2024-03-01'),
-    ('Service B', 'Dr. Johnson', '10:30 AM', '11:30 AM', '2024-03-02'),
-    ('Service C', 'Dr. Anderson', '01:00 PM', '02:00 PM', '2024-03-03'),
-    ('Service D', 'Dr. Brown', '03:30 PM', '04:30 PM', '2024-03-04'),
-    ('Service E', 'Dr. White', '05:00 PM', '06:00 PM', '2024-03-05');
+    ('Service A', 'Dr. Smith', '09:00', '10:00 ', '2024-03-01'),
+    ('Service B', 'Dr. Johnson', '10:30 ', '11:30 ', '2024-03-02'),
+    ('Service C', 'Dr. Anderson', '13:00', '14:00 ', '2024-03-03'),
+    ('Service D', 'Dr. Brown', '15:30', '16:30 ', '2024-03-04'),
+    ('Service E', 'Dr. White', '17:00', '18:00 ', '2024-03-05');
 
 SELECT * FROM bookings;
